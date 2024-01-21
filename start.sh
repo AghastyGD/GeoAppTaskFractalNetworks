a44a36bdf009ada0545f22473d13d6568d2c16e1
AghastyGD <augustodomingosvasco@gmail.com>
2024-01-21 22:26:28 +0200

    fix unrecognized arguments

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@
 
 # And then start app
 python manage.py makemigrations --no-input
-python manage.py migrate --no--input
+python manage.py migrate --no-input
 
 python manage.py runserver 0.0.0.0:8000
